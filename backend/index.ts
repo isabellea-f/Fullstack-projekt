cafbef5e123500cdf102747e603cad3debdfabe6
isabellea-f <isabelle.amsalu@gmail.com>
2024-05-10 13:54:17 +0200

    port config

--- backend/index.ts
+++ backend/index.ts
@@ -1,9 +1,16 @@
 import express, { Request, Response } from "express";
 import path from "path";
 import sqlite3 from "sqlite3";
+import cors from "cors";
 
 const app = express();
 const db = new sqlite3.Database(path.resolve(__dirname, "test.sqlite"));
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
 // Endpoint to fetch all data from the database
 app.get("/categories", (req: Request, res: Response) => {
@@ -20,6 +27,7 @@ app.get("/categories", (req: Request, res: Response) => {
 // Middleware to serve static files from the dist folder
 app.use(express.static(path.join(path.resolve(), "dist")));
 
-app.listen(3000, () => {
-  console.log("Server is running at http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Redo på http://localhost:3000");
 });
