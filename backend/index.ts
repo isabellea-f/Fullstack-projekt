df68899f6cda522cf26330d4e9bd2bb1019da659
isabellea-f <isabelle.amsalu@gmail.com>
2024-05-21 08:12:34 +0200

    test

--- backend/index.ts
+++ backend/index.ts
@@ -1,3 +1,50 @@
+/* import express, { Request, Response } from "express";
+import path from "path";
+import sqlite3 from "sqlite3";
+import cors from "cors";
+
+const app = express();
+const db = new sqlite3.Database(path.resolve(__dirname, "test.sqlite"));
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+
+// FETCH ALL PRODUCTS
+app.get("/product", (req: Request, res: Response) => {
+  db.all("SELECT * FROM product", (err, rows) => {
+    if (err) {
+      console.log(err.message);
+      res.status(500).send("Database error");
+      return;
+    }
+    res.json(rows);
+  });
+});
+
+// FETCH ALL CATEGORIES
+app.get("/categories", (req: Request, res: Response) => {
+  db.all("SELECT * FROM categories", (err, rows) => {
+    if (err) {
+      console.log(err.message);
+      res.status(500).send("Database error");
+      return;
+    }
+    res.json(rows);
+  });
+});
+
+// Middleware to serve static files from the dist folder
+app.use(express.static(path.join(path.resolve(), "dist")));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Redo på http://localhost:3000");
+});
+ */
+
 import express, { Request, Response } from "express";
 import path from "path";
 import sqlite3 from "sqlite3";
@@ -12,7 +59,19 @@ app.use(
   })
 );
 
-// Endpoint to fetch all data from the database
+// FETCH ALL PRODUCTS
+app.get("/product", (req: Request, res: Response) => {
+  db.all("SELECT * FROM product", (err, rows) => {
+    if (err) {
+      console.log(err.message);
+      res.status(500).send("Database error");
+      return;
+    }
+    res.json(rows);
+  });
+});
+
+// FETCH ALL CATEGORIES
 app.get("/categories", (req: Request, res: Response) => {
   db.all("SELECT * FROM categories", (err, rows) => {
     if (err) {
@@ -26,6 +85,11 @@ app.get("/categories", (req: Request, res: Response) => {
 
 // Middleware to serve static files from the dist folder
 app.use(express.static(path.join(path.resolve(), "dist")));
+
+// Catch-all route handler
+app.get("*", (req, res) => {
+  res.sendFile(path.join(path.resolve(), "dist", "index.html"));
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
