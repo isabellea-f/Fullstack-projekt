5e8f98d3922795efc5655a9814aa36f3f6313e9f
isabellea-f <isabelle.amsalu@gmail.com>
2024-05-28 11:14:13 +0200

    changed button props

--- frontend/src/components/MainPage/Cart.tsx
+++ frontend/src/components/MainPage/Cart.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 import "./Cart.css";
 import { useCart } from "../MainPage/CartContent";
+import { Link } from "react-router-dom";
 
 const Cart: React.FC = () => {
   const { cartProducts } = useCart();
@@ -19,7 +20,12 @@ const Cart: React.FC = () => {
       ))}
       <h2>Total: ${total}</h2>
       <Button variant="primary" className="checkout-button">
-        Proceed to Checkout
+        <Link
+          to="/checkout"
+          style={{ color: "inherit", textDecoration: "inherit" }}
+        >
+          Proceed to Checkout
+        </Link>
       </Button>
     </div>
   );
